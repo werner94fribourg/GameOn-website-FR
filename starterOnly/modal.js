e6a1654e233548aca824e9d454978978719986d1
werner94fribourg <werner94@hotmail.com>
2022-07-06 17:34:59 +0200

    Issue #3

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,8 @@ const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
 const closeBtn = document.querySelector('.close');
 const [reserveForm] = document.getElementsByName('reserve');
+const checkboxInput = document.querySelectorAll('.checkbox-input');
+const textControl = document.querySelectorAll('.text-control');
 
 // Variables
 const typesObject = getTypeFields(
@@ -23,19 +25,49 @@ const typesObject = getTypeFields(
   'radio',
   'checkbox'
 );
+
 const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/y;
 const { text: textElement } = typesObject;
 
 // Functions
+const hideInvalidMessage = input => {
+  console.log(input.parentElement);
+  input.parentElement.dataset.errorVisible = false;
+  input.parentElement.dataset.error = '';
+};
+
 const displayInputValid = (valid, input) => {
   if (valid) {
-    console.log(input.value);
+    console.log(input.type === 'checkbox' ? input.checked : input.value);
+    input.parentElement.dataset.errorVisible = false;
+  } else {
+    input.parentElement.dataset.errorVisible = true;
+  }
+};
+
+const displayErrMess = name => {
+  switch (name) {
+    case 'first':
+      return 'Veuillez rentrer un prénom valide (au moins deux charactères).';
+    case 'last':
+      return 'Veuillez rentrer un nom valide (au moins deux charactères).';
+    case 'email':
+      return 'Veuillez rentrer une adresse e-mail valide.';
+    case 'quantity':
+      return "Veuillez sélectionner un nombre d'évenements valide (entre 0 et 99 inclus).";
+    case 'birthdate':
+      return 'Veuillez rentrer une date de naissance valide.';
+    case 'location':
+      return 'Veuillez sélectionner au moins une location existante.';
+    case 'conditions':
+      return "Veuillez accepter les conditions d'utilisation pour pouvoir procéder à l'inscription.";
   }
 };
 
 const assert = (value, errMess, input) => {
   if (!value) {
     console.error(errMess);
+    input.parentElement.dataset.error = displayErrMess(input.name);
     return false;
   }
   return true;
@@ -92,7 +124,7 @@ const assertRadioChecked = inputArray => {
     assert(
       checkedValue !== undefined,
       `One option of ${first.name} field has to be selected.`,
-      checkedValue
+      first
     ),
     checkedValue,
   ];
@@ -136,7 +168,8 @@ const validate = event => {
       case 'radio':
         fields.forEach(data => {
           const [valid, input] = assertRadioChecked(data);
-          displayInputValid(valid, input);
+          const displayInput = input === undefined ? data[0] : input;
+          displayInputValid(valid, displayInput);
         });
         break;
       case 'checkbox':
@@ -144,7 +177,7 @@ const validate = event => {
           const [checkbox1, checkbox2] = Array.from(data);
           const valid = assertCheckBoxChecked(checkbox1);
           displayInputValid(valid, checkbox1);
-          displayInputValid(true, checkbox2);
+          console.log(checkbox2.checked);
         });
     }
   }
@@ -190,3 +223,42 @@ closeBtn.addEventListener('click', () => {
 
 // Submit the form
 reserveForm.addEventListener('submit', validate);
+
+// Check a radio / checkbox field
+checkboxInput.forEach(input => {
+  input.addEventListener('click', event => {
+    hideInvalidMessage(input);
+  });
+});
+
+// Fill a text-control element
+textControl.forEach(input => {
+  input.addEventListener('blur', event => {
+    if (input.parentElement.dataset.errorVisible === 'true') {
+      switch (input.type) {
+        case 'text':
+          console.log(input);
+          console.log(assertRequired(input));
+          console.log(assertMinLength(input));
+          if (assertRequired(input) && assertMinLength(input))
+            hideInvalidMessage(input);
+          break;
+        case 'email':
+          if (assertRequired(input) && assertEmail(input))
+            hideInvalidMessage(input);
+          break;
+        case 'number':
+          if (
+            assertRequired(input) &&
+            assertMinQuantity(input) &&
+            assertMaxQuantity(input)
+          )
+            hideInvalidMessage(input);
+          break;
+        case 'date':
+          if (assertRequired(input)) hideInvalidMessage(input);
+          break;
+      }
+    }
+  });
+});
